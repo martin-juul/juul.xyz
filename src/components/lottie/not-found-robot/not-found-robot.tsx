6fb89d221f0ea2d010e5ff9f5c9bd926129daca2
Martin Juul <code@juul.xyz>
2023-04-13 20:50:56 +0200

    feat: added not found robot animation

---
+++ src/components/lottie/not-found-robot/not-found-robot.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Lottie from 'lottie-react';
+import NotFoundRobotData from './not-found-robot.json';
+
+export function NotFoundRobot() {
+
+  return <Lottie animationData={NotFoundRobotData}/>;
+}
--- src/features/not-found/not-found.tsx
+++ src/features/not-found/not-found.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { NotFoundRobot } from '../../components/lottie/not-found-robot';
 
 export function NotFound() {
 
   return (
     <>
-      <h1><FormattedMessage id="not-found.page.title" /></h1>
-      <p><FormattedMessage id="not-found.page.content" /></p>
+      <h1><FormattedMessage id="not-found.page.title"/></h1>
+      <p><FormattedMessage id="not-found.page.content"/></p>
+
+      <NotFoundRobot />
     </>
-  )
+  );
 }
