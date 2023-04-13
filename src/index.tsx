836876ab15160a85f3d6deb5b6fe7478b44d770b
Martin Juul <code@juul.xyz>
2023-04-13 20:10:24 +0200

    chore: add disclaimer

--- src/index.tsx
+++ src/index.tsx
@@ -21,6 +21,17 @@ root.render(
   </React.StrictMode>,
 );
 
+console.log(`
+  So you like exploring? You're on untested territory. If you edit values manually then it's on you. 🤭
+  I recommend you install React Dev Tools in your browser.
+  The contact form is using https://pageclip.co/ - if you try to pentest it, then report bugs to them.
+  My website is hosted on https://vercel.com - any server related things you should report to them.
+  Otherwise if you happen to find a bug, please do open an issue on my repository at https://github.com/martin-juul/juul.xyz/issues
+  
+  Anything else? Use the contact form.
+  Best regards, Martin.
+`);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
