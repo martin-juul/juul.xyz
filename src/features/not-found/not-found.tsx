1c74968a521142b3302d1c980f48e31fc66975b0
Martin Juul <code@juul.xyz>
2023-04-14 22:40:46 +0200

    fix: don't index not found page

--- src/features/not-found/not-found.tsx
+++ src/features/not-found/not-found.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { NotFoundRobot } from '../../components/lottie/not-found-robot';
 import posthog from 'posthog-js';
+import { Helmet } from 'react-helmet-async';
 
 export function NotFound() {
 
@@ -11,6 +12,10 @@ export function NotFound() {
 
   return (
     <>
+      <Helmet>
+        <meta name="robots" content="noindex" />
+      </Helmet>
+
       <h1><FormattedMessage id="not-found.page.title"/></h1>
       <p><FormattedMessage id="not-found.page.content"/></p>
 
