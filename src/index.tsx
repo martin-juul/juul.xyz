df73df12c789c802c96b4eeff3beb839365f549a
Martin Juul <code@juul.xyz>
2023-04-14 20:09:55 +0200

    feat: add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
 
 import 'thesims.css';
 import './index.css';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import { LanguageContextProvider } from './context/language';
+
+Sentry.init({
+  dsn: 'https://1af453b06b2d43a19604ede64efd2566@o132444.ingest.sentry.io/4505013809446912',
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  // Performance Monitoring
+  tracesSampleRate: 0.2,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -30,7 +41,7 @@ console.log(`
   The contact form is using https://pageclip.co/ - if you try to pentest it, then report bugs to them.
   My website is hosted on https://vercel.com - any server related things you should report to them.
   Otherwise if you happen to find a bug, please do open an issue on my repository at https://github.com/martin-juul/juul.xyz/issues
-  
+
   Anything else? Use the contact form.
   Best regards, Martin.
 `);
